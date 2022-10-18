c799476654c9e06f9df8dc11b44c7c1feef62c36
Joaquin Rives <joaquin.rives@silo.ai>
2022-10-18 14:28:58 +0300

    refactor run pipeline duplicated code

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,13 +1,12 @@
 import subprocess
 import logging
 import pathlib
-import kfp
 import pytest
 import os
 from envsubst import envsubst
 
 from .conftest import HOST_IP
-from .test_kfp import _handle_job_end
+from .test_kfp import run_pipeline
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -57,24 +56,5 @@ def test_run_pipeline_using_registry(tmp_path):
     pipeline_file = tmp_path / "pipeline.yaml"
     render_pipeline_yaml(output=str(pipeline_file))
 
-    client = kfp.Client(host=None)
-
-    created_run = client.create_run_from_pipeline_package(
-        pipeline_file=str(pipeline_file),
-        enable_caching=False,
-        arguments={},
-        run_name="kfp_reg_test_run",
-        experiment_name=EXPERIMENT_NAME,
-    )
-
-    run_id = created_run.run_id
-
-    logger.info(f"Submitted run with ID: {run_id}")
-
-    logger.info(f"Waiting for run {run_id} to complete....")
-    run_detail = created_run.wait_for_run_completion()
-    _handle_job_end(run_detail)
-
-    experiment = client.get_experiment(experiment_name=EXPERIMENT_NAME)
-
-    client.delete_experiment(experiment.id)
+    # submit and run pipeline
+    run_pipeline(pipeline_file=str(pipeline_file), experiment_name=EXPERIMENT_NAME)
